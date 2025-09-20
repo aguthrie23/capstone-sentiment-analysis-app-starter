24fbe07f34fec884cb3f3a39d8fa190a42188060
AdamG <adamhguthrie@gmail.com>
2025-09-19 20:34:49 -0400

    US-01 Implement VADER in the App

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
-    # TODO: Write the code that calls the sentiment analysis functions here.
-    # hint: use request.method == "POST"
+    if request.method == "POST":
+        user_text = request.form.get("user_text")
+        sid = SentimentIntensityAnalyzer()
+        sentiment = sid.polarity_scores(user_text)
+        return render_template('form.html', sentiment=sentiment)
     return render_template('form.html')
 if __name__ == "__main__":
     app.run()
